e48d0f444e75a303e4c0786dc806dc062d771a77
root <hallybzambrano@gmail.com>
2024-05-20 02:15:05 +0800

    Updated social logos in splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:social_fly/screens/splash.dart';
+import 'package:social_fly/utils/screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await Firebase.initializeApp();
   // FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+  ScreenUtil.init();
   runApp(const SocialFly());
 }
 
--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -15,20 +15,15 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   @override
   void initState() {
     super.initState();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-    Future.delayed(const Duration(seconds: 2), () {
-      Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_) => const LoginScreen()),
-      );
-    });
+    // SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
   }
 
   @override
   void dispose() {
-    SystemChrome.setEnabledSystemUIMode(
-      SystemUiMode.manual,
-      overlays: SystemUiOverlay.values,
-    );
+    // SystemChrome.setEnabledSystemUIMode(
+    //   SystemUiMode.manual,
+    //   overlays: SystemUiOverlay.values,
+    // );
     super.dispose();
   }
 
@@ -36,19 +31,161 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        width: double.infinity,
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: AssetImage("assets/images/splash_screen.png"),
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Container(width: double.infinity, height: ScreenUtil.safeBlockVertical * 25, decoration: const BoxDecoration()),
-          ],
+        child: Container(
+          margin: EdgeInsets.symmetric(
+            vertical: ScreenUtil.safeBlockVertical * 8,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                width: double.infinity,
+                height: ScreenUtil.safeBlockVertical * 32,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xfff5ddb5), Color(0x40f5ddb5)],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                child: Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: ScreenUtil.safeBlockVertical * 2,
+                    horizontal: ScreenUtil.safeBlockHorizontal,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'WELCOME TO SOCIALFLY\nA SOCIAL MEDIA NETWORK',
+                        style: TextStyle(
+                          fontSize: ScreenUtil.scaleFactor * 30,
+                          color: const Color(0xfffc3131),
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        'MEET NEW PEOPLE, CHAT, GO LIVE',
+                        style: TextStyle(
+                          fontSize: ScreenUtil.scaleFactor * 24,
+                          color: const Color(0xffcc6ce7),
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                height: ScreenUtil.safeBlockVertical * 32,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Color(0xfff5ddb5), Color(0xaaf5ddb5)],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(
+                        horizontal: ScreenUtil.safeBlockHorizontal * 2,
+                      ),
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        style: ButtonStyle(
+                          padding: MaterialStateProperty.all(EdgeInsets.all(
+                            ScreenUtil.safeBlockHorizontal * 2,
+                          )),
+                          backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(ScreenUtil.safeBlockHorizontal),
+                            ),
+                          ),
+                        ),
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.mail,
+                          color: Colors.white,
+                          size: ScreenUtil.scaleFactor * 48,
+                        ),
+                        label: Text(
+                          'LOGIN OR SIGNUP WITH EMAIL',
+                          style: TextStyle(
+                            fontSize: ScreenUtil.scaleFactor * 20,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: ScreenUtil.safeBlockVertical,
+                    ),
+                    Divider(
+                      indent: ScreenUtil.safeBlockHorizontal * 5,
+                      endIndent: ScreenUtil.safeBlockHorizontal * 5,
+                      thickness: ScreenUtil.safeBlockHorizontal,
+                      color: const Color(0xff935736),
+                    ),
+                    Text(
+                      'OR SIGNUP OR LOGIN WITH',
+                      style: TextStyle(
+                        fontSize: ScreenUtil.scaleFactor * 24,
+                        color: const Color(0xffcc6ce7),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        socialLogo('assets/logos/google.png', 'GOOGLE'),
+                        socialLogo('assets/logos/apple.png', 'APPLE'),
+                        socialLogo('assets/logos/facebook.png', 'FACEBOOK'),
+                        socialLogo('assets/logos/yahoo.png', 'YAHOO'),
+                        socialLogo('assets/logos/microsoft.png', 'MICROSOFT'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+socialLogo(String path, String name) {
+  return Column(
+    children: [
+      Image.asset(
+        path,
+        width: ScreenUtil.safeBlockHorizontal * 13,
+      ),
+      SizedBox(
+        height: ScreenUtil.safeBlockVertical,
+      ),
+      Text(
+        name,
+        style: TextStyle(
+          fontSize: ScreenUtil.scaleFactor * 13,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    ],
+  );
 }
